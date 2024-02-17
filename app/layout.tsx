aadb4c8363db0cff0ec4b5f321c356b3e09b7930
Shrijal <tamrakarshrijal@gmail.com>
2024-02-17 21:57:04 +0545

    Meta Data

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,23 @@ import { Analytics } from '@vercel/analytics/react';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'Shrijal Tamrakar - Homepage',
-  description: 'Shrijal Tamrakar - Homepage',
+  title: 'Shrijal Tamrakar',
+  description: 'Shrijal Tamrakar - Software Engineer',
+  openGraph: {
+    title: 'Shrijal Tamrakar',
+    description: 'Shrijal Tamrakar - Software Engineer',
+    url: 'https://shrijaltamrakar.com.np',
+    siteName: 'Shrijal Tamrakkar',
+    images: [
+      {
+        url: '/assets/shrijal.png', // Must be an absolute URL
+        width: 800,
+        height: 600,
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
 };
 
 export default function RootLayout({
